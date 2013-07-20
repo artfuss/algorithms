3c44685834098b8c3df73ec3a7dca5e0f9794bbd
artfuss <badonisuneet@gmail.com>
2013-07-20 12:47:10 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,9 @@
+def exp(base,exponent):
+    result = 1
+    while (exponent > 0):
+        if (exponent % 2 == 1):
+            result = (result * base)
+        exponent /= 2
+        base = (base * base)
+    return result
+    
