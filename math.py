bb34c909fa341d8405755427acc854ab7b11463d
artfuss <badonisuneet@gmail.com>
2013-07-22 23:12:15 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,3 +1,4 @@
+# exponentiation by repeated squaring, O(log(exponent))
 def exp(base,exponent):
     result = 1
     while (exponent > 0):
@@ -7,3 +8,14 @@ def exp(base,exponent):
         base = (base * base)
     return result
     
+# list of divisors of a number n, O(sqrt(n))
+def divisors(n):
+    facts = []
+    i = 1
+    while i*i <= n:
+        if n%i ==0:
+            facts.append(i)
+            if i*i != n:
+                facts.append(n/i)
+        i += 1
+    return facts
