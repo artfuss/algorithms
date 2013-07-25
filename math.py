dca4fc4ba4c6934a4cb725c1602aefb5f56730a0
artfuss <badonisuneet@gmail.com>
2013-07-26 01:46:55 +0530

    Update math.py

--- math.py
+++ math.py
@@ -7,15 +7,3 @@ def exp(base,exponent):
         exponent /= 2
         base = (base * base)
     return result
-    
-# list of divisors of a number n, O(sqrt(n))
-def divisors(n):
-    facts = []
-    i = 1
-    while i*i <= n:
-        if n%i ==0:
-            facts.append(i)
-            if i*i != n:
-                facts.append(n/i)
-        i += 1
-    return facts
