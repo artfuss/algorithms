5341f180cf8b5fdd0d009a12e029af250ba73b6b
artfuss <badonisuneet@gmail.com>
2013-08-11 20:06:29 +0530

    1.replaced list of ints with list of bytes, reduce memory footprint

--- prime.py
+++ prime.py
@@ -3,25 +3,23 @@ class to build a list of all primes
 below n using sieve of erasthosnes
 '''
 class primes():
-
     def __init__(self,n):
         self.N = n/2
-        self.primes = [1 for __ in xrange(self.N)]
+        self.primes = bytearray(b'1'*self.N)
         i = 1
         while i*i <= self.N+1:
-            if self.primes[i] == 1:
-                for j in xrange(i+2*i+1, self.N, 2*i+1):
-                    self.primes[j]=0
+            if self.primes[i] == 49:
+                for j in xrange(2*i*(i+1), self.N, 2*i+1):
+                    self.primes[j]=48
             i += 1
     def isprime(self,p):
         if p/2 > self.N : raise ValueError
-        return p==2 or ( p>2 and p%2 != 0 and self.primes[p/2]==1)
+        return p==2 or ( p>2 and p%2 != 0 and self.primes[p/2]==49)
 
     def __iter__(self):
         yield 2
         for i in xrange(1,self.N):
-            if self.primes[i]==1: yield 2*i+1
-
+            if self.primes[i]==49: yield 2*i+1
 
 '''
     e.g.1
